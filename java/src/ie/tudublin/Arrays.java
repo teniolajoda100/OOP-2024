7696df65cfb0c39111a05ea68aa82e5d040b524f
Teni <teni@Tenis-MacBook-Air.local>
2024-02-14 20:11:43 +0000

    X and Y axis drawn

--- java/src/ie/tudublin/Arrays.java
+++ java/src/ie/tudublin/Arrays.java
@@ -1,95 +1,124 @@
-/* package ie.tudublin;
+package ie.tudublin;
 
 import processing.core.PApplet;
 
+
+
 public class Arrays extends PApplet
 {
-	String[] months = {"jan", "feb", "mar", "apr","may","june","july","aug","sep","oct","nov","Dec","jan"};
-    float[] rainfall = {200,260,300,150,100,50,10,40,67,160, 400};
-	//Creae bar chart 
-	public float map1(float a, float b, float c, float d, float e)
-	{	
-		//map1
-		float range1 = c - b; 
-		float range2 = e - d;
-		float howfar = a - b;
+	String[] months = {"JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"};
 
-		return d (howfar/range1)*range2;
+	float[] rainfall = {200, 260, 300, 150, 100, 50, 10, 40, 67, 160, 400, 420};
+
+	public float map1(float a, float b, float c, float d, float e)
+	{
+		float r1 = c -b;
+		float r2 = e - d;
+
+		float howFar = a - b;
+		return d + (howFar / r1) * r2;
+	}
+
+	void randomize()
+	{
+		for (int i = 0; i < rainfall.length; i++) {
+			rainfall[i] = random(500);
+		}
 	}
 
 	public void settings()
 	{
-		size(500, 500);
+		size(1100,700);
+
 		String[] m1 = months;
 		months[0] = "XXX";
 		print(m1[0]);
-
-		for (int i = 0; i<months.length; i ++)
+		for(int i = 0; i < months.length; i ++)
 		{
-			println("Month:" + months[i] + "\t" + rainfall[i]);
+			println("Month: " + months[i] + "\t" + rainfall[i]);
+		}
+		for (String s : m1) {
+			println(s);
 		}
 
-		for (String s: m1){
-        println(s);
-		}
-		float min=0;
-		int max = 0;
-		int mini;
-
-		for (int i = 0; i <months.length; i ++)
+		int minIndex = 0;
+		for(int i= 0 ; i < rainfall.length ; i ++)
 		{
-			rainfall[i] = max;
-			if (rainfall[i]>max)
+			if (rainfall[i] < rainfall[minIndex])
 			{
-				rainfall[i] = max;
+				minIndex = i;
 			}
-			
-			min = rainfall[0];
-			
-			if (rainfall[i]<min)
-			{
-				rainfall[i] = min;
-			}
-			 
-	
 		}
-		mini = (int)min;
-
-
-		println(months[max]+"has the most rain"+"\n"+ months[mini]+ "has the least rainfall");
-
-		float total = 0;
-		for (float f:rainfall)
+		
+		int maxIndex = 0;
+		for(int i= 0 ; i < rainfall.length ; i ++)
 		{
-			total +=f;
+			if (rainfall[i] > rainfall[maxIndex])
+			{
+				maxIndex = i;
+			}
 		}
 
-		float avg = total / (float) rainfall.length;
+		println("The month with the minimum rainfall was " + months[minIndex] + " with " + rainfall[minIndex] + "rain");
+		println("The month with the max rainfall was " + months[maxIndex] + " with " + rainfall[maxIndex] + "rain");
+		
+		
+		float tot = 0;
+		for(float f:rainfall)
+		{
+			tot += f;
+		}
 
-		//Draw bar chart
-		println(map1(a:5,b: 0, c:10, d:0, e:100)); //50
-		println(map(a:25,b:20,c:30,d:200,e:300)); //250
-		println(map(a:26, b:25, c:35, d:0, e:100); //10
+		float avg = tot / (float) rainfall.length;
+
+		// a b-c d-e;
+		println(map1(5, 0, 10, 0, 100));
+		// 50
+
+		println(map1(25, 20, 30, 200, 300));
+		// 250
+
+		println(map1(26, 25, 35, 0, 100));
+		// 10 
+
 
 	}
 
 	public void setup() {
 		colorMode(HSB);
 		background(0);
-
+		randomize();
 		
-
 		
 	}
-	
+ 
 	public void draw()
-	{
-		for (int i = 0; i < months.length;i++)
+	{	
+
+		background(250);
+		float w = width / (float)months.length;
+		for(int i = 0 ; i < months.length ;  i ++)
 		{
-			float w = width / months.length;
-			float x = map1(i,b:0,months.length, d:0, width);
-			rect(x,height, -rainfall(i))
-		}	
+			//Variable to shift the bar chart into the axis
+			float n = (float) i + 0.55f;
+			float x = map1(n, 0, months.length, 0, width-30);
+			rect(x, height, w, -rainfall[i]);
+		}
+		//Drawing the axis lines
+		int x1= 65;
+		int y1 = 750;
+		int y2 = 40;
+
+		stroke(250, 0, 0); 
+		line(x1, y1, x1, y2);
+		
+		x1 = 5;
+		//X2 is where he line ends
+		int x2 = 980;
+		//Y1 used to alter where the line  starts from
+		y1 = 640;
+		y2 = 130;
+		line(x1,y1,x2,y1);
+
 	}
 }
- */
